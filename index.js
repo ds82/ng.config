8fe2108b010577d03ede048c26fa9b43e520b615
Jan Hohmann <jhohmann@jnhmn.de>
2015-07-03 21:09:14 +0200

    feat($.load): load external data into config
    load may replaces prior settings.

--- index.js
+++ index.js
@@ -3,4 +3,15 @@
 angular.module('ng-app-config', [])
   .service('$config', Config);
 
-function Config() {}
+function Config() {
+  var vm = this;
+
+  vm.$ = {};
+  vm.$.load = load;
+
+  function load(data) {
+    for (var key in data) {
+      vm[key] = data[key];
+    }
+  }
+}
