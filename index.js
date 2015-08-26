86099ffa444e5a41d5f552a6a4a58dce9c9978ff
Dennis Sänger <mail@dennis.io>
2015-08-26 15:00:34 +0200

    feat(provider): configure module during config phase
    By providing a provider it is possible to configure values during the config
    phase of angulars bootstrap

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 'use strict';
 
-angular.module('ng.config', [])
-  .service('$config', Config);
+var $$get = require('lodash/object/get');
+var $$set = require('lodash/object/set');
 
-function Config() {
+angular.module('ng.config', [])
+  .provider('$config', ConfigProvider);
+
+var service = new ConfigService();
+
+function ConfigProvider() {
+  var vm = this;
+
+  vm.$get = $get;
+  vm.set = service.$.set;
+  vm.get = service.$.get;
+
+  function $get() {
+    return service;
+  }
+}
+
+function ConfigService() {
   var vm = this;
 
   vm.$ = {};
   vm.$.load = load;
+  vm.$.get = get;
+  vm.$.set = set;
+
+  function get(path, def) {
+    return $$get(vm, path, def);
+  }
+
+  function set(path, value) {
+    return $$set(vm, path, value);
+  }
 
   function load(data) {
     for (var key in data) {
